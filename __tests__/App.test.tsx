b76ef495c80801df77a79d0b03a0e00707be12dc
hyeJune <chj950807@naver.com>
2023-11-05 15:40:02 +0900

    Test : React-navigation 관련 Jest 테스트코드 수정

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,16 +1,39 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
+import 'jest';
 // Note: import explicitly to use the types shiped with jest.
 import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      dispatch: jest.fn(),
+    }),
+  };
+});
+
+// include this section and the NativeAnimatedHelper section for mocking react-native-reanimated
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 it('renders correctly', () => {
   renderer.create(<App />);
