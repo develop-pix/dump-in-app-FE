7632d22942facff095763dfc7dbd0d1ebfe2ec2c
hyeJune <chj950807@naver.com>
2023-11-07 17:42:08 +0900

    TEST: Test 코드 수정3

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -8,19 +8,6 @@ import {it} from '@jest/globals';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-import 'react-native-gesture-handler/jestSetup';
-
-jest.mock('@react-navigation/native', () => {
-  const actualNav = jest.requireActual('@react-navigation/native');
-  return {
-    ...actualNav,
-    useNavigation: () => ({
-      navigate: jest.fn(),
-      dispatch: jest.fn(),
-    }),
-  };
-});
-
 it('renders correctly', () => {
   renderer.create(<App />);
 });
