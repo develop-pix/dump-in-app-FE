f9c035789ff957e7e85dfa25d026af85a7d43ed3
SeeYoung Jin <jsee53@naver.com>
2023-12-26 15:39:21 +0900

    Fix: sdk버젼 21->23 수정

--- android/app/src/main/java/com/dump_in_app_fe/MainApplication.java
+++ android/app/src/main/java/com/dump_in_app_fe/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import com.kakao.sdk.common.KakaoSdk;
 
 public class MainApplication extends Application implements ReactApplication {
 
