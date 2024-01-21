cf3d49d9c6a5e0ff287f5af29d8be1daea52df23
seongjun.oh <tiger8708@gmail.com>
2024-01-22 00:28:40 +0900

    FIX: Bundle을 import하여 안드로이드 빌드 오류 수정

--- android/app/src/main/java/com/dump_in_app_fe/MainActivity.java
+++ android/app/src/main/java/com/dump_in_app_fe/MainActivity.java
@@ -5,6 +5,8 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+import android.os.Bundle; 
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -15,12 +17,6 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "dump_in_app_FE";
   }
-
-   // react-native-screens override
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(null);
-    }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
@@ -34,5 +30,11 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
+  // react-native-screens override
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
   }
 }
