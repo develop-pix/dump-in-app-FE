b02d790d8d1db3dd590e1b6259922b9450b11df4
seongjun.oh <tiger8708@gmail.com>
2024-03-07 05:05:24 +0900

    STYLE: SkeletonHome 컴포넌트가 어색하게 출력되는 부분 수정

--- src/components/reuse/skeleton/SkeletonHomeDataCollection.tsx
+++ src/components/reuse/skeleton/SkeletonHomeDataCollection.tsx
@@ -6,40 +6,45 @@ import { colors } from 'styles/base/Variable';
 export default function SkeletonHomeDataCollection() {
     return (
         <SkeletonPlaceholder backgroundColor={colors.blackgrey} highlightColor={colors.lightgrey}>
-            <SkeletonPlaceholder.Item
-                width={Dimensions.get('window').width}
-                flexDirection="row"
-                justifyContent="center">
-                <SkeletonPlaceholder.Item flexDirection="column" gap={10} flexWrap="wrap">
+            <SkeletonPlaceholder.Item flexDirection="row" flexWrap="wrap" justifyContent="space-evenly" paddingTop={16}>
+                <SkeletonPlaceholder.Item flexDirection="column" alignItems="center">
                     <SkeletonPlaceholder.Item
                         width={Dimensions.get('window').width * 0.45}
                         aspectRatio={1}
                         borderRadius={20}
+                        marginBottom={15}
                     />
                     <SkeletonPlaceholder.Item
                         width={Dimensions.get('window').width * 0.45}
                         aspectRatio={0.75}
                         borderRadius={20}
+                        marginBottom={15}
                     />
                     <SkeletonPlaceholder.Item
                         width={Dimensions.get('window').width * 0.45}
                         aspectRatio={0.75}
                         borderRadius={20}
+                        marginBottom={15}
                     />
+                </SkeletonPlaceholder.Item>
+                <SkeletonPlaceholder.Item flexDirection="column" alignItems="center">
                     <SkeletonPlaceholder.Item
                         width={Dimensions.get('window').width * 0.45}
                         aspectRatio={0.75}
                         borderRadius={20}
+                        marginBottom={15}
                     />
                     <SkeletonPlaceholder.Item
                         width={Dimensions.get('window').width * 0.45}
                         aspectRatio={1}
                         borderRadius={20}
+                        marginBottom={15}
                     />
                     <SkeletonPlaceholder.Item
                         width={Dimensions.get('window').width * 0.45}
                         aspectRatio={0.75}
                         borderRadius={20}
+                        marginBottom={15}
                     />
                 </SkeletonPlaceholder.Item>
             </SkeletonPlaceholder.Item>
