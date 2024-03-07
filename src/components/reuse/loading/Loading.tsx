04ed1661f0eb4c3ca9d670e7aec4f0eac81ebc8d
hyeJune <chj950807@naver.com>
2024-03-08 02:19:21 +0900

    FIX: 불필요한 파일 삭제 및 PhotoDump navigation 수정

--- src/components/reuse/loading/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function Loading() {
-    return (
-        <View>
-            <Text>Loading 페이지</Text>
-        </View>
-    );
-}
--- src/components/reuse/modal/PhotoBoothModal.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function PhotoBoothModal() {
-    return (
-        <View>
-            <Text>PhotoBoothModal 페이지</Text>
-        </View>
-    );
-}
