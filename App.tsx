f797dcf7e782b69215895d281e5799693a3b18f6
seongjun.oh <tiger8708@gmail.com>
2024-03-11 19:29:20 +0900

    FEAT: SplashScreen 구현

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import BootSplash from 'react-native-bootsplash';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -10,6 +12,16 @@ import { ScreenProvider } from 'utils/ScreenContext';
 const persistor = persistStore(store);
 
 const App = () => {
+    useEffect(() => {
+        const init = async () => {
+            // …do multiple sync or async tasks
+        };
+
+        init().finally(async () => {
+            await BootSplash.hide({ fade: true });
+        });
+    }, []);
+
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
