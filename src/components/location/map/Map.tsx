33b5d814573b880aa4b0efe09eb21e826b20f26f
hyeJune <chj950807@naver.com>
2024-03-15 03:41:32 +0900

    FIX: 지도페이지 cameraPosition 변경이 안되는 문제, resetLocation 적용이 안되는 문제,  Marker가 출력되지 않던 문제 수정

--- src/components/location/map/Map.tsx
+++ src/components/location/map/Map.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { Animated, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Geolocation from 'react-native-geolocation-service';
 import NaverMapView, { Marker } from 'react-native-nmap';
 import { useDispatch } from 'react-redux';
@@ -8,6 +9,7 @@ import { GetAddressFromNaverGeocoding, GetBranchCardList } from 'hooks/axios/Loc
 import { setCurrentLocation } from 'hooks/redux/currentLocationSlice';
 import { useAppSelector } from 'hooks/redux/store';
 import { BranchCardData, LocationData } from 'interfaces/Location.interface';
+import { LocationStackScreenProps } from 'interfaces/Navigation.interface';
 import { NoBranchContainer, NoBranchToastContainer } from 'styles/layout/location/BranchCarousel.style';
 import { MapContainer } from 'styles/layout/location/Map.style';
 import { FontWhiteSmallerSemibold } from 'styles/layout/reuse/text/Text.style';
@@ -22,20 +24,21 @@ export default function Map() {
     const dispatch = useDispatch();
     const platform = Platform.OS;
     const cardMoveY = useRef(new Animated.Value(0)).current;
+    const navigation = useNavigation<LocationStackScreenProps<'Location'>['navigation']>();
 
     const [location, setLocation] = useState<string>('주소 입력');
     const [branchData, setBranchData] = useState<BranchCardData[]>([]);
     const [zoom, setZoom] = useState<number>(18);
     const [showNearBranch, setShowNearBranch] = useState<boolean>(false);
     const [toastVisible, setToastVisible] = useState<boolean>(false);
-
-    /** 대한민국 북,동,남,서 끝단의 위도 or 경도 */
-    const MAX_COORD = [38.6111111, 131.8695555, 33.11194444, 124.61];
     /** 현재 내가 보고있는 지도의 center */
     const [myPosition, setMyPosition] = useState<LocationData>({
         latitude: 37.564362,
         longitude: 126.977011,
     });
+
+    /** 대한민국 북,동,남,서 끝단의 위도 or 경도 */
+    const MAX_COORD = [38.6111111, 131.8695555, 33.11194444, 124.61];
 
     /**  ReverseGeolocation 호출 */
     const getAddressData = async (latitude: number, longitude: number) => {
@@ -44,8 +47,9 @@ export default function Map() {
     };
 
     /** BranchCard 정보 Get */
+    //TODO: zoom의 크기에따라 radius를 조절할수 해야함. zoom과 radius는 반비례 해야할것 같음. API명세에 명시된 radius 범위가 달라서 확인이 필요함
     const getBranchCardData = async (latitude: number, longitude: number) => {
-        const radius = 1.0;
+        const radius = 0.3;
         try {
             const photoBoothData = await GetBranchCardList(latitude, longitude, radius);
             if (photoBoothData.data) {
@@ -54,10 +58,6 @@ export default function Map() {
         } catch (error) {
             console.log('GetBranchCardDataError ' + error);
         }
-    };
-
-    const ChangePosition = (latitude: number, longitude: number) => {
-        setMyPosition({ ...myPosition, latitude, longitude });
     };
 
     /** 위치 권한 획득 시 redux store에 저장 */
@@ -77,29 +77,31 @@ export default function Map() {
         return watchID;
     };
 
-    /**  Max_COORD 넘어갈시 Map 위치를 서울로 전환 */
-    const ResetCameraPosition = useCallback(
+    /** 지도상에서 지점 클릭시 지점상세 페이지로 이동 */
+    const onClickBranch = (branchID: string) => {
+        navigation.navigate('Branch', { branchID });
+    };
+
+    /** 카메라 위치 변경시 */
+    const ChangePosition = useCallback(
         (latitude: number, longitude: number) => {
-            if (currentLocation.latitude && currentLocation.longitude) {
-                if (longitude > MAX_COORD[0] || latitude < MAX_COORD[2]) {
-                    currentLocation.latitude && currentLocation.longitude
-                        ? setMyPosition({
-                              latitude: currentLocation.latitude,
-                              longitude: currentLocation.longitude,
-                          })
-                        : setMyPosition({ latitude: 37.564362, longitude: 126.977011 });
-                } else if (longitude > MAX_COORD[1] || longitude < MAX_COORD[3]) {
-                    currentLocation.latitude && currentLocation.longitude
-                        ? setMyPosition({
-                              latitude: currentLocation.latitude,
-                              longitude: currentLocation.longitude,
-                          })
-                        : setMyPosition({ latitude: 37.564362, longitude: 126.977011 });
-                }
+            /** 위도가 최북단 보다 크거나 최남단 보다 작을때 (reset) */
+            if (latitude > MAX_COORD[0] || latitude < MAX_COORD[2]) {
+                setMyPosition({
+                    latitude: currentLocation.latitude ? currentLocation.latitude : 37.564362,
+                    longitude: currentLocation.longitude ? currentLocation.longitude : 126.977011,
+                });
+                /** 경도가 최동단 보다 크거나 최서단 보다 작을때 (reset) */
+            } else if (longitude > MAX_COORD[1] || longitude < MAX_COORD[3]) {
+                setMyPosition({
+                    latitude: currentLocation.latitude ? currentLocation.latitude : 37.564362,
+                    longitude: currentLocation.longitude ? currentLocation.longitude : 126.977011,
+                });
+            } else {
+                setMyPosition(prev => ({ ...prev, latitude, longitude }));
             }
         },
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        [],
+        [MAX_COORD, currentLocation.latitude, currentLocation.longitude],
     );
 
     // LocationSearch 페이지로 이동시 위치 권한 획득
@@ -160,7 +162,6 @@ export default function Map() {
                 onCameraChange={e => {
                     setZoom(e.zoom);
                     ChangePosition(e.latitude, e.longitude);
-                    ResetCameraPosition(e.latitude, e.longitude);
                     setShowNearBranch(false);
                 }}
                 onMapClick={() => {
@@ -173,18 +174,17 @@ export default function Map() {
                 minZoomLevel={8}
                 rotateGesturesEnabled={false}
                 tiltGesturesEnabled={false}>
-                {currentLocation.latitude !== null && currentLocation.longitude !== null ? (
+                {currentLocation.latitude !== null && currentLocation.longitude !== null && (
                     <Marker coordinate={{ latitude: currentLocation.latitude, longitude: currentLocation.longitude }} />
-                ) : null}
-
-                {branchData.length > 0
-                    ? branchData.map(position => {
-                          <Marker
-                              coordinate={{ latitude: position.latitude, longitude: position.longitude }}
-                              pinColor="red"
-                          />;
-                      })
-                    : null}
+                )}
+                {branchData.map(branch => (
+                    <Marker
+                        key={branch.id}
+                        coordinate={{ latitude: branch.latitude, longitude: branch.longitude }}
+                        pinColor="red"
+                        onClick={() => onClickBranch(branch.id)}
+                    />
+                ))}
             </NaverMapView>
             <MapInput location={location} />
             {branchData.length > 0 ? (
