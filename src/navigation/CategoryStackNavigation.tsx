034009f363ac358d630f4c040ebab7e532bff070
seongjun.oh <tiger8708@gmail.com>
2024-04-25 00:58:15 +0900

    FIX: Category 탭의 Review Detail 헤더 보이지 않는 오류 수정

--- src/navigation/CategoryStackNavigation.tsx
+++ src/navigation/CategoryStackNavigation.tsx
@@ -28,7 +28,12 @@ export default function CategoryStackNavigation() {
                 initialParams={{ PhotoBoothID: 0 }}
                 options={{ headerShown: true, headerTransparent: true }}
             />
-            <Stack.Screen name="ReviewDetail" component={ReviewDetail} initialParams={{ reviewID: null }} />
+            <Stack.Screen
+                name="ReviewDetail"
+                component={ReviewDetail}
+                initialParams={{ reviewID: null }}
+                options={{ headerShown: true }}
+            />
             <Stack.Screen
                 name="EventDetail"
                 component={EventDetail}
